e5bf3c6e2415537df5e3af4826457e4ea4a1dd6d
rics <guinanaorico@gmail.com>
2023-05-21 21:40:04 +0800

    Edited: Proxy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -89,9 +89,6 @@ const fetchAnswer = async () => {
       
       const res = await fetch('https://chat-cortex-server.vercel.app/api', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
         body: JSON.stringify({
           question: question.value,
         }),
